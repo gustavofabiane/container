643ccf2a43c205ea8fc25ef131a31890dcc2d46b
Gustavo Fabiane <gustavfabiane@gmail.com>
2019-12-15 18:36:34 -0300

    run CS fix

--- tests/Stubs/StubInterface.php
+++ tests/Stubs/StubInterface.php
@@ -2,10 +2,10 @@
 
 /**
  * Container
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- * 
+ *
  * Gustavo Fabiane (c) 2019
  */
 
